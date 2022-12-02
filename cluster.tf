589cb17bb4558b5c321b13fa889ca39359adebd3
Sushma <sushma.gayatri451@gmail.com>
2022-12-02 19:06:10 +0530

    redis

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,58 @@
 # Creates redis (Elastic Cache) Cluster
 
-resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "cluster-example"
-  engine               = "memcached"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 2
-  parameter_group_name = "default.memcached1.4"
-  port                 = 11211
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "cluster-${var.ENV}"
+  engine               = "redis"
+  node_type            = "cache.t3.small"
+  num_cache_nodes      = 1       # An ideal prod cluster should have 3 nodes
+  parameter_group_name = aws_elasticache_parameter_group.default.name
+  engine_version       = "6.x"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids   = [aws_security_group.allow_redis.id]
 }
+
+# Creates Parameter group
+
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "roboshop-redis-${var.ENV}"
+  family = "redis6.x"
+}
+
+# Created subnet Group
+resource "aws_elasticache_subnet_group" "subnet-group" {
+  name       = "roboshop-redis-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+}
+
+# Creates Security group for redis
+
+resource "aws_security_group" "allow_redis" {
+  name        = "roboshop-redis-${var.ENV}"
+  description = "roboshop-redis-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "redis port from def vpc"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-redis-sg-${var.ENV}"
+  }
+}
+
 #  # Creates Docdb Cluster
 
 # resource "aws_docdb_cluster" "docdb" {
